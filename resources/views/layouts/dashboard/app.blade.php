db681d6fcb53cbb388193a4fa1d91261dcbe5699
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-11-16 08:40:46 +0200

    Add admin-theme

---
+++ resources/views/layouts/dashboard/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+<head>
+   @include('layouts.dashboard.includes.head')
+</head>
+<body class="vertical-layout vertical-menu-modern 2-columns   menu-expanded fixed-navbar"
+      data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
+@include('layouts.dashboard.includes.nav')
+
+@include('layouts.dashboard.includes.side-bar')
+
+@yield('content')
+
+
+@include('layouts.dashboard.includes.footer')
+
+@include('layouts.dashboard.includes.script')
+</body>
+</html>
