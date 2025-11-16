1030abf46f0343902d6dc4323c41dc4b7c55d04b
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-11-16 05:50:24 +0200

    start project

---
+++ README.md
@@ -0,0 +1,2 @@
+https://erdplus.com/diagrams/163850 => relation table
+https://erdplus.com/diagrams/163825  => digram tables
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ], function () {
+
+
+});
