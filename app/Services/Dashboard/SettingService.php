59a0b0520d766fe654a8ce5c1df84941ea6168cc
Mohammed-tareq <mohtareq1999m@gmail.com>
2026-01-02 16:25:44 +0200

    Add settings management module with CRUD functionality, repository-service structure, multilingual support, middleware, and AJAX integration.

--- app/Services/Dashboard/SettingService.php
+++ app/Services/Dashboard/SettingService.php
@@ -3,11 +3,13 @@
 namespace App\Services\Dashboard;
 
 use App\Repositories\Dashboard\SettingRepository;
+use App\Utils\ImageManagement;
 
 class SettingService
 {
 
-    public function __construct(protected SettingRepository $settingRepository)
+    public function __construct(protected SettingRepository $settingRepository,
+                                protected ImageManagement $imageManagement)
     {
     }
 
@@ -18,6 +20,16 @@ class SettingService
 
     public function updateSetting($data)
     {
+        if(!empty($data['logo'])){
+            $this->imageManagement->deleteImageFromLocal($data['logo']);
+            $data['logo'] = $this->imageManagement->uploadSingleImage('/',$data['logo'],'settings');
+        }
+
+        if(!empty($data['favicon'])){
+            $this->imageManagement->deleteImageFromLocal($data['favicon']);
+            $data['favicon'] = $this->imageManagement->uploadSingleImage('/',$data['favicon'],'settings');
+        }
+
         return $this->settingRepository->updateSetting($data);
     }
 
