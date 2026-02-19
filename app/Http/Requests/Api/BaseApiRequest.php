dae05e64eeb87cb83a39a7456f69f15e1fae2686
Mohammed-tareq <mohtareq1999m@gmail.com>
2026-02-19 20:30:06 +0200

    Add BaseApiRequest: centralize request validation and custom JSON error responses for API requests.

---
+++ app/Http/Requests/Api/BaseApiRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+       if(request()->expectsJson()){
+           $response = response()->json([
+               'success' => false,
+               'message' => 'Validation error',
+               'errors' => $validator->errors()->messages()
+           ], 422);
+
+           throw new HttpResponseException($response);
+       }
+    }
+}
